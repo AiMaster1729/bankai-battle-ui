2d71eb8cb272194d74b37dea47711bc064801a58
AiMaster1729 <tbprasad369@gmail.com>
2025-04-14 23:27:51 +0530

    Update bankais.tsx

--- src/data/bankais.tsx
+++ src/data/bankais.tsx
@@ -38,7 +38,7 @@ export const bankais: BankaiType[] = [
     description: "Byakuya's Senbonzakura Kageyoshi in TYBW creates billions of tiny blades that move at his will, forming a dome of lethal cherry blossoms that cut enemies to pieces.",
     shikai: "Senbonzakura",
     bankai: "Senbonzakura Kageyoshi",
-    imageSrc: "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?auto=format&fit=crop&w=600&q=80",
+    imageSrc: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/318df4da-1f4d-472c-84b4-74c73f492824/dew2wl5-2e068d75-3e0d-4d71-a0f6-1b4aeb856ee9.jpg/v1/fill/w_1192,h_670,q_70,strp/byakuya_by_waywardheronstairs_dew2wl5-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvMzE4ZGY0ZGEtMWY0ZC00NzJjLTg0YjQtNzRjNzNmNDkyODI0XC9kZXcyd2w1LTJlMDY4ZDc1LTNlMGQtNGQ3MS1hMGY2LTFiNGFlYjg1NmVlOS5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.pM2O_N6Tv81kbwRh60KBvOXLRh-R-r5s58_9_yYLCR8",
     reiatsuColor: "reiatsu-purple",
   },
   {
